2b359f0ae02459368cb00a296d7139acf318a219
ClarkTheSup <lknkoqq123@163.com>
2020-09-20 13:42:23 +0800

    [kangning] feat: add @ActiveProfiles(test) in test Class

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,32 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class DemoSpringConfigApplicationTests {
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mockMvc.perform(get("/level"))
+				.andExpect(jsonPath("$", is("advanced")))
+				.andExpect(status().isOk());
 	}
 
 }
