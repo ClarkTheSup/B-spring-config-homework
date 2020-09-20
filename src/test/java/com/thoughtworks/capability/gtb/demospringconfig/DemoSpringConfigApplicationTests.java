9eb78dcc5440c26d8c81bccdd518ff955462b45a
ClarkTheSup <lknkoqq123@163.com>
2020-09-20 13:45:13 +0800

    [kangning] fix: remove useless imports

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,12 +1,10 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -14,7 +12,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
